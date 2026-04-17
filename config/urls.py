a4d63384828ac1252bf2167eba514c51bc159f4c
Daniil Bachev <abobus2029@gmail.com>
2026-04-17 12:17:23 +0500

    feat: настроил маршрутизацию жанго и добавил рабочий index.html

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
